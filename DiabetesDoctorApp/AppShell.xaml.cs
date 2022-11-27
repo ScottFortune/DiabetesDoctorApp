e7b7945d81732534c9dfcce55c3f4731b368d970
ScottFortune <scottfortunejr20@gmail.com>
2022-11-27 09:46:59 -0500

    Initial commit

---
+++ DiabetesDoctorApp/AppShell.xaml.cs
@@ -0,0 +1,24 @@
+﻿using DiabetesDoctorApp.ViewModels;
+using DiabetesDoctorApp.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace DiabetesDoctorApp
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+            Routing.RegisterRoute(nameof(Diabetes101Page), typeof(Diabetes101Page));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ DiabetesDoctorApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using DiabetesDoctorApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace DiabetesDoctorApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
